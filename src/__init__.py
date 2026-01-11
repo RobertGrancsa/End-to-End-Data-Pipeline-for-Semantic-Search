4116797c42496251117de19e0a57cd55981f647f
Rares Constantin <rares.constantin2002@gmail.com>
2026-01-11 14:21:32 +0200

    Rares: Add project config module

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+"""
+Source Package Initialization
+
+End-to-End Data Pipeline for Semantic Search
+"""
+
+__version__ = "1.0.0"
+__author__ = "Team: Andrei-Daniel Anghelescu, Rares-Alexandru Constantin, Robert Grancsa, Ana-Maria Toader"
