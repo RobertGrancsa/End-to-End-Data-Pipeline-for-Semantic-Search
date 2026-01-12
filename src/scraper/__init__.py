0988694eb38ab9744f9f45aaa34bb824f5086461
Rares Constantin <rares.constantin2002@gmail.com>
2026-01-12 13:06:41 +0200

    Rares: Implement web scraper with rate limiting

---
+++ src/scraper/__init__.py
@@ -0,0 +1,11 @@
+"""
+Scraper Package
+
+Developed by: Rares-Alexandru Constantin
+Feature: Web Crawler for Data Aggregation
+"""
+
+from .web_scraper import WebScraper
+from .content_extractor import ContentExtractor
+
+__all__ = ["WebScraper", "ContentExtractor"]
